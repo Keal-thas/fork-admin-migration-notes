1d9bf14693ff2804e2209de8b3c007b12bd6062a
Keal-thas <dec1503409765@gmail.com>
2026-08-03 21:24:30 +0800

    Fill in real schema names (SALESYS, SALESYSFLOW)

--- cross_schema_dependency_check.sql
+++ cross_schema_dependency_check.sql
@@ -5,15 +5,15 @@ FROM all_constraints a
 JOIN all_constraints c
   ON a.r_constraint_name = c.constraint_name AND a.r_owner = c.owner
 WHERE a.constraint_type = 'R'
-  AND a.owner IN ('SCHEMA1', 'SCHEMA2')
-  AND c.owner IN ('SCHEMA1', 'SCHEMA2')
+  AND a.owner IN ('SALESYS', 'SALESYSFLOW')
+  AND c.owner IN ('SALESYS', 'SALESYSFLOW')
   AND a.owner != c.owner;
 
 -- Cross-schema synonyms (including PUBLIC synonyms pointing at either schema)
 SELECT owner, synonym_name, table_owner, table_name
 FROM all_synonyms
-WHERE table_owner IN ('SCHEMA1', 'SCHEMA2')
-  AND (owner IN ('SCHEMA1', 'SCHEMA2') AND owner != table_owner
+WHERE table_owner IN ('SALESYS', 'SALESYSFLOW')
+  AND (owner IN ('SALESYS', 'SALESYSFLOW') AND owner != table_owner
        OR owner = 'PUBLIC');
 
 -- Broadest check: any object (view/procedure/trigger/package/etc.) in one
@@ -21,6 +21,6 @@ WHERE table_owner IN ('SCHEMA1', 'SCHEMA2')
 SELECT owner, name, type AS referencing_type,
        referenced_owner, referenced_name, referenced_type
 FROM all_dependencies
-WHERE owner IN ('SCHEMA1', 'SCHEMA2')
-  AND referenced_owner IN ('SCHEMA1', 'SCHEMA2')
+WHERE owner IN ('SALESYS', 'SALESYSFLOW')
+  AND referenced_owner IN ('SALESYS', 'SALESYSFLOW')
   AND owner != referenced_owner;
